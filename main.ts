b24a933eb6cf088130f7010663f6dc15780e8d0c
AngleIT <4162608@qq.com>
2022-02-22 14:37:42 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     rContent = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
-    if (isBluetoothConnected && (rContent && rContent.trim().length > 0)) {
+    if (rContent) {
+        rContent = rContent.trim()
+    }
+    if (isBluetoothConnected && rContent) {
         basic.showString(rContent)
         basic.showNumber(rContent.length)
         basic.pause(2000)
@@ -18,14 +21,45 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () 
 })
 bluetooth.onBluetoothConnected(function () {
     isBluetoothConnected = true
+    bluetooth.uartWriteString("connection succeeded")
     basic.showIcon(IconNames.Yes)
 })
 input.onButtonPressed(Button.A, function () {
+    isPressedButtonB = false
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    bluetooth.uartWriteString("IR ))) send close")
     if (isBluetoothConnected) {
-    	
+        isPressedButtonA = true
+        bluetooth.uartWriteString("IR ((( receive open")
+        pins.digitalWritePin(DigitalPin.P15, 1)
     }
 })
+input.onButtonPressed(Button.B, function () {
+    isPressedButtonA = false
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    bluetooth.uartWriteString("IR ((( receive close")
+    if (isBluetoothConnected) {
+        isPressedButtonB = true
+        bluetooth.uartWriteString("IR ))) send open")
+    }
+    pins.digitalWritePin(DigitalPin.P14, 1)
+})
+// 接收红外信号
+pins.onPulsed(DigitalPin.P15, PulseValue.High, function () {
+	
+})
+// 发送红外信号
+pins.onPulsed(DigitalPin.P14, PulseValue.High, function () {
+	
+})
 let isBluetoothConnected = false
+let isPressedButtonA = false
+let isPressedButtonB = false
 let rContent = ""
-isBluetoothConnected = false
+let IRData = [] as Array<any>
+isPressedButtonB = false
+isPressedButtonA = false
+rContent = ""
+pins.digitalWritePin(DigitalPin.P14, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
 basic.showIcon(IconNames.Heart)
